39171da4aedc68a41438353c9af792578e5eb946
Server\NOT/founD <purplefenix228@mail.ru>
2025-10-01 13:00:21 +0700

    Added page and resources

---
+++ ModulesPR3/Pages/Btns.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace ModulesPR3.Pages
+{
+    public partial class Btns : Page
+    {
+        public Btns()
+        {
+            InitializeComponent();
+        }
+    }
+}
