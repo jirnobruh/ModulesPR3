f58d1c70ef1dd7e9352f824ec8bb3617aaae6184
Server\NOT/founD <purplefenix228@mail.ru>
2025-11-20 22:41:24 +0700

    PR3 and PR5 combined into one solution

--- ModulesPR3/Pages/Auth.xaml.cs
+++ ModulesPR3/Pages/Auth.xaml.cs
@@ -1,6 +1,11 @@
 using System;
 using System.Windows;
 using System.Windows.Controls;
+using HashPasswords;
+using ModulesPR3.Services;
+using ModulesPR5.Models;
+using ModulesPR5;
+using System.Linq;
 
 namespace ModulesPR3.Pages
 {
@@ -15,12 +20,37 @@ namespace ModulesPR3.Pages
         {
             try
             {
-                NavigationService.Navigate(new Clients());
+                string login = txtbLogin.Text.Trim();
+                string password = Hash.HashPassword(txtbPassword.Password.Trim());
+
+                var db = Helper.GetContext();
+
+                var auth = db.Auth.FirstOrDefault(x => x.login == login && x.password == password);
+                if (auth != null)
+                {
+                    var user = db.Applicants.FirstOrDefault(x => x.auth_id == auth.id);
+                    var role = db.Roles.FirstOrDefault(x => x.id == auth.role_id);
+                    if (auth != null)
+                    {
+                        MessageBox.Show("Вы вошли под: " + role.Title.ToString());
+                        LoadPage(role.Title.ToString());
+                    }
+                    else
+                    {
+                         MessageBox.Show("Вы ввели логин или пароль неверно!");
+                         GenerateCaptcha();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Введите данные заново!");
+                    GenerateCaptcha();
+                }
             }
             catch (Exception exception)
             {
                 Console.WriteLine(exception);
-                throw;
+                MessageBox.Show(exception.Message);
             }
         }
 
@@ -34,6 +64,26 @@ namespace ModulesPR3.Pages
             {
                 Console.WriteLine("btnAuthGuest error:", exception);
                 throw;
+            }
+        }
+
+        private void GenerateCaptcha()
+        {
+            tbCaptcha.Visibility = Visibility.Visible;
+            txtbCaptcha.Visibility = Visibility.Visible;
+            
+            string captchaText = CaptchaGenerator.GenerateCaptchaText(6);
+            tbCaptcha.Text = captchaText;
+            tbCaptcha.TextDecorations = TextDecorations.Strikethrough;
+        }
+        
+        private void LoadPage(string _role)
+        {
+            switch (_role)
+            {
+                case "Applicant":
+                    NavigationService.Navigate(new Clients());
+                    break;
             }
         }
     }
